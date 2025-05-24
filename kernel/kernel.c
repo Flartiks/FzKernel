a6649bdcac20dc84902bdf66d92b2c50d778c89d
Flartiks <duofreeze598@gmail.com>
2025-05-25 01:12:09 +0300

    initial commit, add shell with some commands

---
+++ kernel/kernel.c
@@ -0,0 +1,46 @@
+#include "graphics.h"
+#include "pit.h"
+#include "ports.h"
+#include "pic.h"
+#include "idt.h"
+#include "multiboot.h"
+#include "shell.h"
+#include "keyboard.h"  
+
+extern uint32_t stack_space;
+
+void kernel_main() {
+
+    clear_screen(make_color(COLOR_WHITE, COLOR_BLACK));
+
+    pic_remap(0x20, 0x28);    
+    init_idt();               
+    init_keyboard();          
+    init_pit(100);            
+    asm volatile ("sti");     
+
+    print("[PRELOAD OS]", make_color(COLOR_RED,COLOR_BLACK));
+    print(" ---> ", make_color(COLOR_LIGHT_BLUE, COLOR_BLACK));
+    sleep(10); 
+    print("Welcome in FzKernel\n", make_color(COLOR_LIGHT_RED, COLOR_BLACK));
+
+    print("[PRELOAD OS]", make_color(COLOR_RED,COLOR_BLACK));
+    sleep(10);
+    print(" In future there will be loads of drivers and etc \n", make_color(COLOR_LIGHT_GREEN, COLOR_BLACK));
+
+	sleep(50);
+
+    print("[KERNEL]", make_color(COLOR_LIGHT_RED,COLOR_BLACK));
+    print(" ---> ", make_color(COLOR_LIGHT_BLUE, COLOR_BLACK));
+    sleep(10);
+    print("Kernel loaded! Opening shell in: ", make_color(COLOR_LIGHT_GREEN, COLOR_BLACK));
+
+    sleep(50);
+    print("3 ", make_color(COLOR_LIGHT_GREEN, COLOR_BLACK));
+    sleep(50);
+    print("2 ", make_color(COLOR_LIGHT_GREEN, COLOR_BLACK));
+    sleep(50);
+    print("1 \n", make_color(COLOR_LIGHT_GREEN, COLOR_BLACK));
+	sleep(75);
+    launch_shell();  
+}
